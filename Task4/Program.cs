f89868cefd2eec40f0db534c2999520fe4f716ab
Nik <ice_fire50@mail.ru>
2023-02-10 15:56:34 +0300

    end task4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c = int.Parse(Console.ReadLine());
+int max=0;
+if (a>max)
+    max=a;
+if (b>max)
+    max=b;
+if (c>max)
+    max=c;
+Console.WriteLine("ответ: "+max);
